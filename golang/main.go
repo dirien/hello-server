4178b549181f810343af955db0ee625be5d87878
Engin Diri <engin.diri@ediri.de>
2022-12-07 12:51:27 +0100

    feat: add golang-server (#1)

---
+++ golang/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	// Port is the port the server will listen on
+	Port = "8080"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello World!\n"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", YourHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = Port
+	}
+	fmt.Println("Listening on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
