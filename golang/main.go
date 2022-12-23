640f69487b14a077a96e9c049c172c391debba48
Engin Diri <engin.diri@ediri.de>
2022-12-23 11:48:17 +0100

    feat: print env vars on startup and via restapi (#7)

--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const (
@@ -35,15 +36,37 @@ func RandomFileGenerator(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func PrintEnvByName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	envVar := vars["envVar"]
+	if os.Getenv(envVar) != "" {
+		w.WriteHeader(http.StatusOK)
+		enVarResult := fmt.Sprintf("%s=%s", envVar, os.Getenv(envVar))
+		_, err := w.Write([]byte(enVarResult))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", YourHandler)
 	r.HandleFunc("/file/{fileName}", RandomFileGenerator)
+	r.HandleFunc("/env/{envVar}", PrintEnvByName)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = Port
 	}
-	fmt.Println("Listening on port " + port)
+
+	for _, env := range os.Environ() {
+		if strings.HasPrefix(env, "TEST") {
+			log.Printf("%s", env)
+
+		}
+	}
+
+	log.Println("Listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
